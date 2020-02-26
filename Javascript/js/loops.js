c522b693ef1abb36a92c455c3e968e819ae5ee70
ScottTomassetti <scott_t_ct@hotmail.com>
2020-02-26 06:54:32 -0500

    finished loops.js functions

--- Javascript/js/loops.js
+++ Javascript/js/loops.js
@@ -93,19 +93,150 @@
 
 //Speed Limit program example
 
-checkSpeed(130);
+// checkSpeed(130);
 
-function checkSpeed(speed) {
-   const speedLimit = 70;
-   const kmPerPoint = 5;
+// function checkSpeed(speed) {
+//    const speedLimit = 70;
+//    const kmPerPoint = 5;
 
-   if (speed < speedLimit + kmPerPoint)
-      console.log('OK');
-   else {
-      const points = Math.floor((speed - speedLimit) / kmPerPoint);
-      if (points >= 12)
-         console.log('Licence suspended');
-      else
-         console.log('Points', points)
-   }
+//    if (speed < speedLimit + kmPerPoint)
+//       console.log('OK');
+//    else {
+//       const points = Math.floor((speed - speedLimit) / kmPerPoint);
+//       if (points >= 12)
+//          console.log('Licence suspended');
+//       else
+//          console.log('Points', points)
+//    }
+// }
+
+//************************ */
+
+//odd and even numbers to 10
+
+// showNumbers(10);
+
+// function showNumbers(limit) {
+//    for (let i = 0; i <= limit; i++) {
+//       const message = (i % 2 === 0) ? 'Even' : 'Odd'
+//       console.log(i, message);
+//    }
+  
+// }
+
+//*********************** */
+
+//count the number of truthy statements
+
+// const array = [0, null, undefined, 1, 2, 3, '', 5];
+
+// console.log("The number of truthy values in the array is: ", countTruthy(array));
+
+// function countTruthy(array) {
+//    let count = 0;
+//    for (let value of array)
+//       if(value)
+//          count++;
+//    return count;
+// }
+
+//************************ */
+
+//disply only string properties in an Object
+
+// const movie = {
+//    title: 'a',
+//    releaseYear: 2018,
+//    rating: 4.5,
+//    director: 'b'
+// };
+
+// showProperties(movie);
+
+// function showProperties(obj) {
+//    for (let key in obj)
+//       if (typeof obj[key] === 'string')
+//          console.log(key, obj[key]);
+// }
+
+//************************ */
+
+//sum number that are multiples of 3 or 5
+
+// console.log(sum(10));
+
+// function sum(limit) {
+//    let sum = 0;
+
+//    for (let i = 0; i <= limit; i++)
+//       if (i % 3 ===0 || i % 5 === 0)
+//          sum += i;
+
+//    return sum;
+// }
+
+//********************* */
+
+//Grade students
+
+// const marks = [ 80, 80, 90];
+
+// //Average = 70
+
+// //1-59: F
+// //60-69: D
+// //70-79: C
+// //80-89: B
+// //90-100: A
+
+// console.log(calculateGrade(marks));
+
+// function calculateGrade(marks) {
+//    const aveage = calculateAverage(marks);
+//    if (aveage < 60) return 'F';
+//    if (aveage < 70) return 'D';
+//    if (aveage < 80) return 'C';
+//    if (aveage < 90) return 'B';
+//    return 'A';
+// }
+
+// function calculateAverage(array) {
+//    let sum = 0;
+//    for (let value of array)
+//       sum +=value;
+//    return sum / array.length;
+// }
+
+//****************************** */
+
+//show stars example
+
+// showStars(5);
+
+// function showStars(rows) {
+//    for (let row = 1; row <= rows; row++) {
+//       let pattern = '';
+//       for (let i = 0; i < row; i++)
+//          pattern += '*';
+//       console.log(pattern);
+//    }
+// }
+//************************ */
+//Show only Prime Numbers Example
+
+showPrimes(20)
+
+function showPrimes(limit) {
+   for (let number = 2; number <= limit; number++) 
+      if (isPrime(number)) console.log(number);
+
+ 
+//isPrime checks to see if a number is prime or not
+}
+function isPrime(number) {
+
+   for (let factor = 2; factor < number; factor++)
+      if (number % factor ===0) 
+         return false;
+   return true;
 }
